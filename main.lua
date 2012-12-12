46ccf6608c8a8eadc57f8cb189e9b6b2f7c5cc01
jbert <jbert@salsa.(none)>
2012-12-11 19:32:09 -0800

    updated main

--- main.lua
+++ main.lua
@@ -1,8 +1,38 @@
+move = 1
+
+function create_player(self,filename,x,y)
+	self = {
+		placement = {
+			x     = x,
+			y     = y
+		},
+		image      = {
+			body   = love.graphics.newImage(filename),
+		}
+	}
+	width  = self.image.body:getWidth()
+    height = self.image.body:getHeight()
+	return self
+end
+
 function love.load()
+	player = create_player(player, 'largeVLC.png', 10, 10)
 end
 
 function love.update(dt)
+	player.placement.x = player.placement.x + move
+end
+
+function love.keypressed(key)
+	if key == ' ' then
+		move = move * -1
+	end
 end
 
 function love.draw()
+	love.graphics.draw(
+		player.image.body,
+		player.placement.x,
+		player.placement.y
+	)
 end
