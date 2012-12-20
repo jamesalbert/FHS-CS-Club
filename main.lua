dc6f67e89c3b5f85f7a9f02984ca95ece2043ec3
jbert <james.albert72@gmail.com>
2012-12-19 23:42:43 -0800

    fixed jump

--- main.lua
+++ main.lua
@@ -5,6 +5,9 @@ hoc = require 'hoc'
 
 detect = hoc(100, player_collide, player_dismiss)
 move = 0
+jump = 0
+jump_on = 0
+jump_off = 1
 
 function big_bang(self, filename)
 	self = {
@@ -27,32 +30,33 @@ function init_player(self, filename)
 		w       = 38,
 		h       = 38,
 		speed   = 0,
+		jump    = 0,
 		image   = {
 			sprite  = love.graphics.newImage(filename)
---			change_to = function (frame)
---				if frame == 'default' then
---					love.graphics.drawq(self.image.sprite, self.image.default, 200+self.speed, 385)
---				elseif frame == 'back' then
---					love.graphics.drawq(self.image.sprite, self.image.back, 200+self.speed, 385)
---				end
---			end
 		},
 		collision = {},
 		update_player = function (image, x, y)
 			love.graphics.draw(image, x, y)
 		end
 	}
-	self.image.default = love.graphics.newQuad( 76, 38, self.w, self.h, 419, 270 )
-	self.image.back = love.graphics.newQuad( 190, 228, self.w, self.h, 419, 270 )
+	self.image.width = self.image.sprite:getWidth()
+	self.image.height = self.image.sprite:getHeight()
+	self.image.default = love.graphics.newQuad( 76, 38, self.w, self.h, self.image.width, self.image.height )
+	self.image.back = love.graphics.newQuad( 190, 228, self.w, self.h, self.image.width, self.image.height )
+	self.image.jump = love.graphics.newQuad( 38, 38, self.w, self.h, self.image.width, self.image.height ) 
 	self.animation = newAnimation(self.image.sprite, 38, 38, 0.1, 6)
 	self.collision.form = detect:addRectangle(
 		self.x, self.y, self.w, self.h
 	)
 	self.image.change_to = function (frame)
+		new_x = 200+self.speed
+		new_y = 385+jump
         if frame == 'default' then
-            love.graphics.drawq(self.image.sprite, self.image.default, 200+self.speed, 385)
+            love.graphics.drawq(self.image.sprite, self.image.default, new_x, new_y)
         elseif frame == 'back' then
-            love.graphics.drawq(self.image.sprite, self.image.back, 200+self.speed, 385)
+            love.graphics.drawq(self.image.sprite, self.image.back, new_x, new_y)
+		elseif frame == 'jump' then
+			love.graphics.drawq(self.image.sprite, self.image.jump, new_x, new_y)
         end 
     end
 	return self
@@ -79,22 +83,45 @@ function player_dismiss()
 end
 
 function get_key()
+	move = move - 2
+	player.speed = player.speed + 2
 	if move < 1 and move > -2020 then
 		if love.keyboard.isDown('a') then
 			player.image.change_to('back')
 			move = move + 2
 			player.speed = player.speed - 2
+		elseif love.keyboard.isDown(' ') then
+			player.image.change_to('jump')
+			if jump > -36 and jump_off == -1 then
+				jump = jump - 4
+				jump_on = 1
+			end
+			if jump == -36 then
+				jump_off = 1
+				jump_on = 0
+			end	
+			if jump ~= 0 and jump_on == 0 then
+                jump = jump + 2
+				if jump == 0 then
+					jump_off = 1
+				end
+            end
+			if jump == 0 then
+				--jump_off = 0
+			end
 		else
 			player.image.change_to('default')
-			move = move - 2
-            player.speed = player.speed + 2
 		end
 	elseif move >= 1 then
 		move = 0
 		player.speed = 0
 	elseif move <= -2020 then
 		--move = move + 2
+
 		--player.speed = player.speed - 2
+	end
+	if jump ~= 0 then
+    	jump = jump + 2
 	end
 end
 
@@ -107,6 +134,9 @@ function love.load()
 end
 
 function love.keypressed(key)
+	if key == ' ' then
+		jump_off = jump_off * -1
+	end
 end
 
 function love.keyreleased(key)
